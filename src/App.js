3fe679a153e70a8eecd80bfdbe78f419590fcc4a
Dima Zeklam <dema.ziklam@gmail.com>
2021-08-16 15:05:23 +0300

    adding error handling functionality

--- src/App.js
+++ src/App.js
@@ -12,24 +12,35 @@ class App extends React.Component {
       CityData: {},
       cityLoc: '',
       mapData: '',
+      errorHandl: false,
       show: false
     };
   }
   getLocation = async (e) => {
     console.log('work');
     e.preventDefault();
-    await this.setState({
-      cityLoc: e.target.input.value
-    });
-    let locationUrl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_KEY}&q=${this.state.cityLoc}&format=json`;
 
-    let Data = await axios.get(locationUrl);
+    try {
 
-    await this.setState({
-      CityData: Data.data[0],
-      show: true
-    });
 
+      await this.setState({
+        cityLoc: e.target.input.value
+      });
+
+      let locationUrl = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_API_KEY}&q=${this.state.cityLoc}&format=json`;
+
+      let Data = await axios.get(locationUrl);
+
+      await this.setState({
+        CityData: Data.data[0],
+        show: true
+      });
+    } catch (error) {
+      await this.setState({
+        show: false,
+        errorHandl: true
+      });
+    }
     let mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_API_KEY}&center=${this.state.CityData.lat},${this.state.CityData.lon}&zoom=16&size=480x480&markers=icon:large-red-cutout|${this.state.CityData.lat},${this.state.CityData.lon}&markers=icon:large-red-cutout|${this.state.CityData.lat},${this.state.CityData.lon}&path=fillcolor:%23add8e6|weight:1|color:blue|${this.state.CityData.lat},${this.state.CityData.lon}|${this.state.CityData.lat},${this.state.CityData.lon}${this.state.CityData.lat},${this.state.CityData.lon}|${this.state.CityData.lat},${this.state.CityData.lon}|${this.state.CityData.lat},${this.state.CityData.lon}`;
 
     this.setState({
@@ -38,7 +49,11 @@ class App extends React.Component {
     });
 
   }
-
+  close = () => {
+    this.setState({
+      errorHandl: false
+    });
+  }
   render() {
     return (
 
@@ -52,21 +67,33 @@ class App extends React.Component {
         </form> */}
           <Form onSubmit={this.getLocation}>
             <Form.Group className="mb-3" controlId="formBasicEmail" >
-              <Form.Label style={{ size:'25px',fontWeight:'bold',padding:'30px'}}>Type the location</Form.Label>
-              <Form.Control type="text" placeholder="amman" name='input' style={{ position:'relative', left:'35%',width: '30%'}} />
+              <Form.Label style={{ size: '25px', fontWeight: 'bold', padding: '30px' }}>Type the location</Form.Label>
+              <Form.Control type="text" placeholder="amman" name='input' style={{ position: 'relative', left: '35%', width: '30%' }} />
             </Form.Group>
 
             <Button variant="primary" type="submit">
-            Explore!
+              Explore!
             </Button>
           </Form>
           {
             this.state.show &&
-            <p style={{ size:'25px',fontWeight:'bold',padding:'30px'}}>{this.state.cityLoc} Lat:{this.state.CityData.lat} / Lon:{this.state.CityData.lon}</p>
+            <p style={{ size: '25px', fontWeight: 'bold', padding: '30px' }}>{this.state.cityLoc} Lat:{this.state.CityData.lat} / Lon:{this.state.CityData.lon}</p>
 
           }
           {this.state.show &&
             <img src={this.state.mapData} />
+          }
+          {
+            this.state.errorHandl &&
+
+      
+            <div class="alert alert-warning alert-dismissible fade show" role="alert">
+              <strong>Error! try to set the location name </strong>
+              <button onClick={this.close} type="button" style={{ marginLeft:'20px', fontWeight:'bold' }} data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+
           }
         </div>
       </>
